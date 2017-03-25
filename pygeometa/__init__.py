f297c0a70cbe21a0554237eedb37c4467d8f5ede
Tom Kralidis <tomkralidis@gmail.com>
2017-03-25 16:46:20 +0000

    update release version to 0.2.1

--- pygeometa/__init__.py
+++ pygeometa/__init__.py
@@ -57,7 +57,7 @@ from jinja2.exceptions import TemplateNotFound
 from six import StringIO
 from six.moves.configparser import ConfigParser
 
-__version__ = '0.2.0'
+__version__ = '0.2.1'
 
 LOGGER = logging.getLogger(__name__)
 
