62298e0cad05e449e58e1ee6989418eaa534eae6
Tom Kralidis <tomkralidis@gmail.com>
2019-06-11 11:11:41 +0000

    remove Python 2 support

--- setup.py
+++ setup.py
@@ -84,12 +84,7 @@ def get_package_version():
     raise RuntimeError("Unable to find version string.")
 
 
-try:
-    import pypandoc
-    LONG_DESCRIPTION = pypandoc.convert('README.md', 'rst')
-except(IOError, ImportError, OSError):
-    print('Conversion to rST failed.  Using default (will look weird on PyPI)')
-    LONG_DESCRIPTION = read('README.md')
+LONG_DESCRIPTION = read('README.md')
 
 DESCRIPTION = 'pygeometa is a Python package to generate metadata for geospatial datasets'  # noqa
 
@@ -101,6 +96,7 @@ setup(
     version=get_package_version(),
     description=DESCRIPTION.strip(),
     long_description=LONG_DESCRIPTION,
+    long_description_content_type='text/markdown',
     license='MIT',
     platforms='all',
     keywords=' '.join([
