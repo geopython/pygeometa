83a7c08549f14cfd72e4c551b08f340fed23e5f5
Niccolo Cantu <niccolo.cantu@nina.no>
2025-04-24 10:13:50 +0200

    import gbif_eml

--- pygeometa/schemas/__init__.py
+++ pygeometa/schemas/__init__.py
@@ -56,6 +56,7 @@ SCHEMAS = {
     'cwl': 'pygeometa.schemas.cwl.CWLOutputSchema',
     'csvw': 'pygeometa.schemas.csvw.CSVWOutputSchema',
     'dcat': 'pygeometa.schemas.dcat.DCATOutputSchema',
+    'gbif-eml': 'pygeometa.schemas.gbif_eml.GBIF_EMLOutputSchema',
     'iso19139': 'pygeometa.schemas.iso19139.ISO19139OutputSchema',
     'iso19139-2': 'pygeometa.schemas.iso19139_2.ISO19139_2OutputSchema',
     'iso19139-hnap': 'pygeometa.schemas.iso19139_hnap.ISO19139HNAPOutputSchema',  # noqa
