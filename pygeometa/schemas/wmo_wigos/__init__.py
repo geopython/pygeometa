67f1454b8f72ad2d46cd9660926639a445a7f2e7
Tom Kralidis <tomkralidis@gmail.com>
2025-09-04 20:31:33 -0400

    test WMDR JSON

--- pygeometa/schemas/wmo_wigos/__init__.py
+++ pygeometa/schemas/wmo_wigos/__init__.py
@@ -18,7 +18,7 @@
 # those files. Users are asked to read the 3rd Party Licenses
 # referenced with those assets.
 #
-# Copyright (c) 2020 Tom Kralidis
+# Copyright (c) 2025 Tom Kralidis
 #
 # Permission is hereby granted, free of charge, to any person
 # obtaining a copy of this software and associated documentation
@@ -44,10 +44,22 @@
 # =================================================================
 
 import os
+from typing import Union
+
+from lxml import etree
 
 from pygeometa.schemas.base import BaseOutputSchema
 
 THISDIR = os.path.dirname(os.path.realpath(__file__))
+
+NAMESPACES = {
+    'gco': 'http://www.isotc211.org/2005/gco',
+    'gmd': 'http://www.isotc211.org/2005/gmd',
+    'gml': 'http://www.opengis.net/gml/3.2',
+    'om': 'http://www.opengis.net/om/2.0',
+    'wmdr': 'http://def.wmo.int/wmdr/2017',
+    'xlink': 'http://www.w3.org/1999/xlink'
+}
 
 
 class WMOWIGOSOutputSchema(BaseOutputSchema):
@@ -63,3 +75,118 @@ class WMOWIGOSOutputSchema(BaseOutputSchema):
         description = 'WMO WIGOS Metadata Standard'
 
         super().__init__('wmo-wigos', description, 'xml', THISDIR)
+
+    def import_(self, metadata: str) -> dict:
+        """
+        Import metadatra into MCF
+
+        :param metadata: `str` of metadat content
+
+        :returns: `dict` of MCF content
+        """
+
+        mcf = {
+            'mcf': {
+                'version': '1.0',
+            },
+            'metadata': {
+                'charset': 'utf8',
+            },
+            'identification': {},
+            'contact': {},
+            'distribution': {},
+            'facility': {}
+        }
+
+        self.exml = etree.fromstring(metadata)
+
+        mcf['metadata']['identifier'] = self._get_xpath_value('//wmdr:facility/wmdr:ObservingFacility/gml:identifier')
+        mcf['metadata']['datestamp'] = self._get_xpath_value('//wmdr:headerInformation//wmdr:fileDateTime')
+
+        contact_xpaths = [
+            '//wmdr:recordOwner/gmd:CI_ResponsibleParty',
+            '//wmdr:facility/wmdr:ObservingFacility//gmd:CI_ResponsibleParty'
+        ]
+
+        for contact_xpath in contact_xpaths:
+            role, contact = self._get_contact(contact_xpath)
+            mcf['contact'][role] = contact
+
+        return mcf
+
+    def _get_xpath_value(self, xpath: str,
+                         node: etree._Element = None) -> Union[str, list]:
+        """
+        Helper function to get a value by XPath
+
+        :param xpath: `str` of XPath expression
+        :param node: `etree._Element` of node
+
+        :returns: `str` or `list` of matching values
+        """
+
+        node2 = self.exml
+
+        if node is not None:
+            node2 = node
+
+        values = node2.xpath(xpath, namespaces=NAMESPACES)
+
+        if len(values) == 1:
+            if hasattr(values[0], 'text'):
+                return values[0].text
+            else:
+                return str(values[0])
+        else:
+            return [value.text for value in values]
+
+        return values
+
+    def _get_contact(self, xpath: str) -> tuple:
+        """
+        Helper function to get contact information by XPath
+
+        :param xpath: `str` of XPath expression
+
+        :returns: `dict` of contact information
+        """
+
+        contact = {}
+        role = None
+
+        val = self._get_xpath_value(f'{xpath}/gmd:role/gmd:CI_RoleCode/@codeListValue')
+        if val:
+            role = val
+        else:
+            role = 'host'
+
+        val = self._get_xpath_value(f'{xpath}/gmd:individualName/gco:CharacterString')
+        if val:
+            contact['individualname'] = val
+
+        val = self._get_xpath_value(f'{xpath}/gmd:organisationName/gco:CharacterString')
+        if val:
+            contact['organization'] = val
+
+        val = self._get_xpath_value(f'{xpath}//gmd:deliveryPoint/gco:CharacterString')
+        if val:
+            contact['address'] = val
+
+        val = self._get_xpath_value(f'{xpath}//gmd:city/gco:CharacterString')
+        if val:
+            contact['city'] = val
+
+        val = self._get_xpath_value(f'{xpath}//gmd:administrativeArea/gco:CharacterString')
+        if val:
+            contact['adminstrativearea'] = val
+
+        val = self._get_xpath_value(f'{xpath}//gmd:country/gco:CharacterString')
+        if val:
+            contact['country'] = val
+
+        val = self._get_xpath_value(f'{xpath}//gmd:electronicMailAddress/gco:CharacterString')
+        if val:
+            contact['email'] = val[0]
+
+
+        return role, contact
