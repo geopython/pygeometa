a16e8e90a5f7b68b9a64b1a2c9b0800b131e4f9e
Tom Kralidis <tomkralidis@gmail.com>
2025-11-03 22:38:52 -0500

    back to dev

--- pygeometa/__init__.py
+++ pygeometa/__init__.py
@@ -49,7 +49,7 @@ import click
 from pygeometa.core import (generate, import_, info, schemas,
                             transform, validate)
 
-__version__ = '0.19.0'
+__version__ = '0.20.dev0'
 
 
 @click.group()
